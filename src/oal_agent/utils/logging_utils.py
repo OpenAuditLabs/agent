540df5ce31fba480881bcb5894c9f65cd3085501
Ross Williamson <rosswilliamson166@gmail.com>
2025-10-09 06:40:55 +0000

    Fix: Update logger handler levels when setting logger level

---
+++ src/oal_agent/utils/logging_utils.py
@@ -0,0 +1,17 @@
+import logging
+from typing import Optional
+
+def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        logger.setLevel(level)
+        for h in logger.handlers:
+            h.setLevel(level)
+        return logger
+
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s %(name)s: %(message)s"))
+    logger.addHandler(handler)
+    logger.propagate = False
+    return logger
