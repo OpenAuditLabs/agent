0fb342403e71d93dcb4e7f5db1ce6e5e8526e047
Rob Aribas <aribasrob@gmail.com>
2025-12-02 18:18:01 -0700

    Add basic Locust load test for analysis endpoint (#221)

---
+++ tests/load/locustfile.py
@@ -0,0 +1,23 @@
+from locust import HttpUser, task, between
+
+class AnalysisUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def submit_analysis(self):
+        contract_code = """
+        pragma solidity ^0.8.0;
+
+        contract SimpleStorage {
+            uint public storedData;
+
+            function set(uint x) public {
+                storedData = x;
+            }
+
+            function get() public view returns (uint) {
+                return storedData;
+            }
+        }
+        """
+        self.client.post("/analysis/", json={"contract_code": contract_code})
